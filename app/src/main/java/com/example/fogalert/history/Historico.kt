1b9ad7b634dd51ed16451f904fa3b9953710b861
20222tiot0292 <ifeseduarda@gmail.com>
2025-06-16 15:02:34 -0300

    Criação das telas: Histórico Semanal e Configurações

--- app/src/main/java/com/example/fogalert/history/Historico.kt
+++ app/src/main/java/com/example/fogalert/history/Historico.kt
@@ -1,18 +1,20 @@
-package com.seuprojeto.fogalert.ui
+package com.example.fogalert.history
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.seuprojeto.fogalert.R
+import com.example.fogalert.R
 
-class Home : Fragment() {
+
+class Historico : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_historico, container, false)
     }
+
 }
